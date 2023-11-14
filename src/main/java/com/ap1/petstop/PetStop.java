6c41486aa40341731c66433c3a68c405d5ae2194
Fierda Stephen <fierdastephen14@gmail.com>
2023-11-14 22:05:19 +0700

    first commit
    first commit

---
+++ src/main/java/com/ap1/petstop/PetStop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ap1.petstop;
+
+/**
+ *
+ * @author SINGKAREK
+ */
+public class PetStop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
